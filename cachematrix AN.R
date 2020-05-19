4e882169513bc21c84cacd2bddaab56960af1180
TheFishGuyNamedNanninga <63072198+TheFishGuyNamedNanninga@users.noreply.github.com>
2020-05-19 07:33:05 -0700

    Add files via upload

---
+++ cachematrix AN.R
@@ -0,0 +1,32 @@
+##  matrix object function for caching its inverse.
+
+makeCacheMatrix <- function(x = matrix()) {
+  j <- NULL
+  set <- function(y){
+    x <<- y
+    j <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) j <<- inverse
+  getInverse <- function() j 
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
+}
+
+##  Computes inverse of above matrix results, if already calculated then function should 
+## retrieve the inverse from the cache
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  j <- x$getInverse()
+  if(!is.null(j)){
+    message("getting cached data")
+    return(j)
+  }
+  mat <- x$get()
+  j <- solve(mat,...)
+  x$setInverse(j)
+  j
+}  
+
